43062c12898435d8847db2d411097dda06dad0c3
= <=>
2024-01-17 00:33:28 +0300

    Refactored timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -15,7 +15,7 @@ void timer(int h, int m)
 	std::lock_guard<std::mutex> lock(myMutex);
 	while (true)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::seconds(1));
 		std::system("cls");
 
 		std::chrono::minutes time;
